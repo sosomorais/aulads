2e8b75f2e9c594a112ba8b9e3ff23112f9a720dc
sofia.morais <sofia.morais@aluno.edu.pi.gov.br>
2026-06-16 19:21:02 +0000

    objeto.js

---
+++ objeto.js
@@ -0,0 +1,17 @@
+const pessoa= {
+    nome: "maria",
+    idade: 18,
+    peso: 47,
+    altura: 1.68,
+    sexo: "feminino"
+}
+
+    console.log(pessoa.nome);
+    pessoa.idade= 19
+    console.log(pessoa.idade)
+    console.log(pessoa.peso)
+    pessoa.peso= 45
+    console.log(pessoa.altura)
+    pessoa.altura=1,50
+    console.log(pessoa.sexo)
+    pessoa.sexo = "masculino"
